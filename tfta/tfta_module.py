88c32ae0e5f8307fe08b1a022a33875204d9fa43
Xue Zhang <xue.zhang@tufts.edu>
2017-07-28 16:53:32 -0400

    Update tfta_module.py

--- tfta/tfta_module.py
+++ tfta/tfta_module.py
@@ -2,6 +2,7 @@
 and to other agents in the system"""
 
 import sys
+import xml.etree.ElementTree as ET
 import logging
 logging.basicConfig(format='%(levelname)s: %(name)s - %(message)s',
 		    level=logging.INFO)
