c4001771e497ebc462d923a4e76330593c4c397b
Xue Zhang <xue.zhang@tufts.edu>
2017-08-09 16:09:50 -0400

    Update tfta_module.py

--- tfta/tfta_module.py
+++ tfta/tfta_module.py
@@ -973,7 +973,8 @@ def _get_pathway_name(target_str):
     pathway_name = []
     try:
         for term in root.find('TERM').find('drum-terms').findall('drum-term'):
-	    pathway_name = pathway_name + [term.get('matched-name')]
+	    if term.get('matched-name') is not None:
+	        pathway_name = pathway_name + [term.get('matched-name')]
 	
 	pathway_name = list(set(pathway_name))
 	print 'pathway_name=' + ','.join(pathway_name)
