258db8022908487e2bdf432a1080f494d68801d1
XZhang <xue.zhang@tufts.edu>
2019-05-30 16:14:07 -0400

    complete test for is-regulation

--- tfta/test/test1_json.py
+++ tfta/test/test1_json.py
@@ -9,9 +9,9 @@ from indra.statements import Agent
 from bioagents import Bioagent
 
 #####################################
-#Testing the following TFTA functions
-#IS-REGULATION
-#
+# Testing the following TFTA capabilities
+# IS-REGULATION
+# FIND-TF
 #
 #
 #
@@ -19,26 +19,26 @@ from bioagents import Bioagent
 ######################################
 
 def _get_targets(target_arg):
-        proteins = None
-        family = None
-        agents = Bioagent.get_agent(target_arg)
-        if isinstance(agents, list):
-            proteins = [a.name for a in agents if a is not None and ('UP' in a.db_refs or 'HGNC' in a.db_refs)]
-            family = [a.name for a in agents if a is not None and 'FPLX' in a.db_refs and a.name not in proteins]
-        elif isinstance(agents, Agent):
-            if 'UP' in agents.db_refs or 'HGNC' in agents.db_refs:
-                proteins = [agents.name]
-            if not proteins and 'FPLX' in agents.db_refs:
-                family = [agents.name]
-        if proteins:
-            print('genes=', ','.join(proteins))
-        else:
-            print('Genes = None\n')
-        if family:
-            print('family=', ','.join(family))
-        else:
-            print('family = None\n')
-        return proteins,family
+    proteins = None
+    family = None
+    agents = Bioagent.get_agent(target_arg)
+    if isinstance(agents, list):
+        proteins = [a.name for a in agents if a is not None and ('UP' in a.db_refs or 'HGNC' in a.db_refs)]
+        family = [a.name for a in agents if a is not None and 'FPLX' in a.db_refs and a.name not in proteins]
+    elif isinstance(agents, Agent):
+        if 'UP' in agents.db_refs or 'HGNC' in agents.db_refs:
+            proteins = [agents.name]
+        if not proteins and 'FPLX' in agents.db_refs:
+            family = [agents.name]
+    if proteins:
+        print('genes=', ','.join(proteins))
+    else:
+        print('Genes = None\n')
+    if family:
+        print('family=', ','.join(family))
+    else:
+        print('family = None\n')
+    return proteins,family
         
 #############################################################################
 #TEST IS-REGULATION
@@ -64,9 +64,9 @@ class TestIsRegulation1(_IntegrationTest):
 
 #protein family
 #Does stat3 regulate the SMURF gene?
-class TestIsRegulation11(_IntegrationTest):
+class TestIsRegulation2(_IntegrationTest):
     def __init__(self, *args):
-        super(TestIsRegulation11, self).__init__(TFTA_Module)
+        super(TestIsRegulation2, self).__init__(TFTA_Module)
 
     def create_message(self):
         # Here we create a KQML request that the TFTA needs to respond to
@@ -84,4 +84,247 @@ class TestIsRegulation11(_IntegrationTest):
         print("len(output.get('clarification'))=", len(output.get('clarification')))
         assert output.get('reason') == 'FAMILY_NAME', output
         assert len(output.get('clarification')) == 5, output
+        
+#Does stat3 regulate the MEK gene?
+class TestIsRegulation3(_IntegrationTest):
+    def __init__(self, *args):
+        super(TestIsRegulation3, self).__init__(TFTA_Module)
+
+    def create_message(self):
+        # Here we create a KQML request that the TFTA needs to respond to
+        tf = agent_clj_from_text('STAT3')
+        _get_targets(tf)
+        print('tf=', str(tf))
+        target_arg = agent_clj_from_text('MEK')
+        _get_targets(target_arg)
+        print('target=', str(target_arg))
+        _get_targets(target_arg)
+        content = KQMLList('IS-REGULATION')
+        content.set('tf', tf)
+        content.set('target', target_arg)
+        return get_request(content), content
+        
+    def check_response_to_message(self, output):
+        assert output.head() == 'FAILURE', output
+        print("len(output.get('clarification'))=", len(output.get('clarification')))
+        assert output.get('reason') == 'FAMILY_NAME', output
+        assert len(output.get('clarification')) == 5, output
+        
+#Does stat3 regulate the AKT gene?
+class TestIsRegulation4(_IntegrationTest):
+    def __init__(self, *args):
+        super(TestIsRegulation4, self).__init__(TFTA_Module)
+
+    def create_message(self):
+        # Here we create a KQML request that the TFTA needs to respond to
+        tf = agent_clj_from_text('STAT3')
+        _get_targets(tf)
+        print('tf=', str(tf))
+        target_arg = agent_clj_from_text('AKT')
+        _get_targets(target_arg)
+        print('target=', str(target_arg))
+        _get_targets(target_arg)
+        content = KQMLList('IS-REGULATION')
+        content.set('tf', tf)
+        content.set('target', target_arg)
+        return get_request(content), content
+        
+    def check_response_to_message(self, output):
+        assert output.head() == 'FAILURE', output
+        print("len(output.get('clarification'))=", len(output.get('clarification')))
+        assert output.get('reason') == 'FAMILY_NAME', output
+        assert len(output.get('clarification')) == 5, output
+
+##Does AKT regulate the MEK gene? (subtask: is-tf-target)
+class TestIsRegulation5(_IntegrationTest):
+    def __init__(self, *args):
+        super(TestIsRegulation5, self).__init__(TFTA_Module)
+
+    def create_message(self):
+        # Here we create a KQML request that the TFTA needs to respond to
+        tf = agent_clj_from_text('AKT')
+        _get_targets(tf)
+        print('tf=', str(tf))
+        target = agent_clj_from_text('MEK')
+        _get_targets(target)
+        print('target=', str(target))
+        content = KQMLList('IS-REGULATION')
+        content.set('tf', tf)
+        content.set('target', target)
+        return get_request(content), content
+        
+    def check_response_to_message(self, output):
+        assert output.head() == 'FAILURE', output
+        print("len(output.get('clarification'))=", len(output.get('clarification')))
+        assert output.get('reason') == 'FAMILY_NAME', output
+        assert len(output.get('clarification')) == 5, output
+
+##does srf regulate acta1?
+class TestIsRegulation6(_IntegrationTest):
+    def __init__(self, *args):
+        super(TestIsRegulation6, self).__init__(TFTA_Module)
+
+    def create_message(self):
+        # Here we create a KQML request that the TFTA needs to respond to
+        tf = agent_clj_from_text('srf')
+        _get_targets(tf)
+        print('tf=', str(tf))
+        target = agent_clj_from_text('acta1')
+        _get_targets(target)
+        print('target=', str(target))
+        content = KQMLList('IS-REGULATION')
+        content.set('tf', tf)
+        content.set('target', target)
+        #print(content, '\n')
+        return get_request(content), content
+        
+    def check_response_to_message(self, output):
+        assert output.head() == 'SUCCESS', output
+        print("len(output.get('result'))=", str(len(output.get('result'))))
+        print('result=', output.get('result'))
+        print('db=', output.get('db'))
+        print('literature=', output.get('literature'))
+        assert output.get('result') == 'TRUE', output
+        assert output.get('db') == 'TRUE', output
+
+#Does STAT3 regulate the JUN gene in the lung? (subtask: is-tf-target-tissue)
+class TestIsRegulation7(_IntegrationTest):
+    def __init__(self, *args):
+        super(TestIsRegulation7, self).__init__(TFTA_Module)
+        
+    def create_message(self):
+        # Here we create a KQML request that the TFTA needs to respond to
+        #since trips parser cannot recognize JUN as a gene, so instead try
+        #c-JUN or JUN gene
+        tf = agent_clj_from_text('STAT3')
+        _get_targets(tf)
+        print('tf=', str(tf))
+        target = agent_clj_from_text('c-JUN')
+        _get_targets(target)
+        print('target=', str(target))
+        tissue = 'lung'
+        content = KQMLList('IS-REGULATION')
+        content.set('tf', tf)
+        content.set('target', target)
+        content.set('tissue', tissue)
+        return get_request(content), content
+        
+    def check_response_to_message(self, output):
+        assert output.head() == 'SUCCESS', output
+        assert output.gets('result') == 'TRUE', output
+
+#Does STAT3 regulate the c-fos gene in the liver? (subtask: is-tf-target-tissue)
+class TestIsRegulation8(_IntegrationTest):
+    def __init__(self, *args):
+        super(TestIsRegulation8, self).__init__(TFTA_Module)
+        
+    def create_message(self):
+        # Here we create a KQML request that the TFTA needs to respond to
+        tf = agent_clj_from_text('STAT3')
+        _get_targets(tf)
+        print('tf=', str(tf))
+        target = agent_clj_from_text('c-fos')
+        _get_targets(target)
+        print('target=', str(target))
+        tissue = 'liver'
+        content = KQMLList('IS-REGULATION')
+        content.set('tf', tf)
+        content.set('target', target)
+        content.set('tissue', tissue)
+        return get_request(content), content
+        
+    def check_response_to_message(self, output):
+        assert output.head() == 'SUCCESS', output
+        assert output.gets('result') == 'FALSE', output
+
+#Does AKT regulate the c-fos gene in the lung? (subtask: is-tf-target-tissue)
+class TestIsRegulation9(_IntegrationTest):
+    def __init__(self, *args):
+        super(TestIsRegulation9, self).__init__(TFTA_Module)
+        
+    def create_message(self):
+        # Here we create a KQML request that the TFTA needs to respond to
+        tf = agent_clj_from_text('AKT')
+        _get_targets(tf)
+        print('tf=', str(tf))
+        
+        target = agent_clj_from_text('c-fos')
+        _get_targets(target)
+        print('target=', str(target))
+        
+        tissue = 'lung'
+        content = KQMLList('IS-REGULATION')
+        content.set('tf', tf)
+        content.set('target', target)
+        content.set('tissue', tissue)
+        return get_request(content), content
+        
+    def check_response_to_message(self, output):
+        assert output.head() == 'FAILURE', output
+        assert output.get('reason') == 'FAMILY_NAME', output
+        print("len(output.get('clarification'))=", len(output.get('clarification')))
+        assert len(output.get('clarification')) == 5, output
+
+#Does STAT3 regulate the c-fos gene in the breast cancer? (subtask: is-tf-target-tissue)
+class TestIsRegulation10(_IntegrationTest):
+    def __init__(self, *args):
+        super(TestIsRegulation10, self).__init__(TFTA_Module)
+        
+    def create_message(self):
+        # Here we create a KQML request that the TFTA needs to respond to
+        tf = agent_clj_from_text('STAT3')
+        _get_targets(tf)
+        print('tf=', str(tf))
+        
+        target = agent_clj_from_text('c-fos')
+        _get_targets(target)
+        print('target=', str(target))
+        
+        tissue = 'breast cancer'
+        content = KQMLList('IS-REGULATION')
+        content.set('tf', tf)
+        content.set('target', target)
+        content.set('tissue', tissue)
+        return get_request(content), content
+        
+    def check_response_to_message(self, output):
+        assert output.head() == 'FAILURE', output
+        assert output.gets('reason') == 'INVALID_TISSUE', output
+
+#keyword
+#Does STAT3 increase transcription of the c-fos gene?
+class TestIsRegulation11(_IntegrationTest):
+    def __init__(self, *args):
+        super(TestIsRegulation11, self).__init__(TFTA_Module)
+
+    def create_message(self):
+        # Here we create a KQML request that the TFTA needs to respond to
+        tf = agent_clj_from_text('stat3')
+        _get_targets(tf)
+        print('tf=', str(tf))
+        
+        target = agent_clj_from_text('c-fos')
+        _get_targets(target)
+        print('target=', str(target))
+        
+        keyword = 'increase'
+        content = KQMLList('IS-REGULATION')
+        content.set('tf', tf)
+        content.set('target', target)
+        content.set('keyword', keyword)
+        print(content, '\n')
+        return get_request(content), content
+        
+    def check_response_to_message(self, output):
+        assert output.head() == 'SUCCESS', output
+        print("len(output.get('result'))=", str(len(output.get('result'))))
+        print('result=', output.get('result'))
+        print('db=', output.get('db'))
+        print('literature=', output.get('literature'))
+        assert output.get('result') == 'TRUE', output
+        assert output.get('literature') == 'TRUE', output
+        assert output.get('db') == 'FALSE', output
+
+##############################################################################
+#
 
